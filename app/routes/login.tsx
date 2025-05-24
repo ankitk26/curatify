2a7db01a3fb351126d78a93855daa8d3114ddee4
ankitk26 <myselfankit51@gmail.com>
2025-05-25 05:24:15 +0530

    update login page ui

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -8,16 +8,24 @@ export const Route = createFileRoute("/login")({
 
 function RouteComponent() {
   return (
-    <div>
-      <Button
-        onClick={async () => {
-          await authClient.signIn.social({
-            provider: "spotify",
-          });
-        }}
-      >
-        Log in
-      </Button>
+    <div className="w-screen h-screen flex items-center justify-center">
+      <div className="border p-16 rounded-xl flex flex-col items-center">
+        <h1 className="text-4xl font-semibold">curatify</h1>
+        <p className="mt-2 text-muted-foreground">
+          Manage your Spotify playlists with ease
+        </p>
+        <Button
+          className="mt-8"
+          size="lg"
+          onClick={async () => {
+            await authClient.signIn.social({
+              provider: "spotify",
+            });
+          }}
+        >
+          Log in
+        </Button>
+      </div>
     </div>
   );
 }
