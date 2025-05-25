67a309320f5f60c908cf3706c8d2c4042b0700c8
ankitk26 <myselfankit51@gmail.com>
2025-05-25 06:47:37 +0530

    add back button

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,6 +1,6 @@
 import { useSuspenseQuery } from "@tanstack/react-query";
-import { useNavigate } from "@tanstack/react-router";
-import { LogOutIcon } from "lucide-react";
+import { useCanGoBack, useNavigate, useRouter } from "@tanstack/react-router";
+import { ArrowLeftIcon, LogOutIcon } from "lucide-react";
 import { authClient } from "~/lib/auth-client";
 import { query } from "~/queries";
 import { Avatar, AvatarImage } from "./ui/avatar";
@@ -9,14 +9,29 @@ import { Input } from "./ui/input";
 
 export default function Header() {
   const navigate = useNavigate();
+  const router = useRouter();
+
   const { data } = useSuspenseQuery(query.users.me);
+  const canGoBack = useCanGoBack();
 
   return (
     <header className="flex items-center justify-between gap-16">
-      <Input
-        className="flex-1"
-        placeholder="Search playlists, albums, artists and tracks"
-      />
+      <div className="flex items-center gap-4 flex-1">
+        <Button
+          disabled={!canGoBack}
+          variant="outline"
+          size="icon"
+          onClick={() => {
+            router.history.back();
+          }}
+        >
+          <ArrowLeftIcon />
+        </Button>
+        <Input
+          className="flex-1"
+          placeholder="Search playlists, albums, artists and tracks"
+        />
+      </div>
 
       <div className="flex items-center gap-4">
         <Avatar>
